94482ef3904dce5bc750fd957eb8ff8fe3009228
SojeongLee <kather0220@ewhain.net>
2021-11-11 02:44:37 +0900

    feat: add header component

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,1 +1,38 @@
-//헤더
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = () => {
+  return (
+    <Wrapper>
+      <StyledLogo src="logo.png"></StyledLogo>
+      <SearchInputBox></SearchInputBox>
+    </Wrapper>
+  );
+};
+
+export default Header;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledLogo = styled.img`
+  width: 6em;
+  height: 6em;
+  padding-top: 2em;
+`;
+
+export const SearchInputBox = styled.input`
+  background: white;
+  border: 1px solid #dddddd;
+  border-radius: 2em;
+  margin: 1em 1em;
+  width: 18em;
+  color: black;
+  font-size: 2em;
+  &::placeholder {
+    color: lightgrey;
+  }
+`;
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+import Header from '../../components/common/header';
 
-const main =() => {
- return(<div>메인화면</div>)
-}
+const main = () => {
+  return (
+    <Wrapper>
+      <Header></Header>
+    </Wrapper>
+  );
+};
 
 export default main;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
